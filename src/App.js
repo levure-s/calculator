3fe532400fb6be1cc49053484194c73e8efc8abd
levure-s <levure.sauvage@gmail.com>
2021-04-28 11:48:05 +0900

    first_commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Button from './Button';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {count:0}
+    
+  }
+
+  handleClick(value){
+    this.setState({count:this.state.count + parseInt(value)})
+  }
+  
+  render() {
+    const list =[
+      {value:"1"},
+      {value:"2"},
+      {value:"3"},
+      {value:"4"},
+      {value:"5"},
+      {value:"6"},
+      {value:"7"},
+      {value:"8"},
+      {value:"9"}
+    ]
+    return (
+      <div>
+        <h1>
+          {this.state.count}
+          
+        </h1>
+
+        {list.map((listItem)=>{
+          return(
+            <button onClick = {()=>{this.handleClick(listItem.value)}}><Button number = {listItem.value}/></button>
+          )
+        })}
+        
+        
+        
+      </div>
+    );
+  }
 }
 
 export default App;
